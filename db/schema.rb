883fa0dd6e76d47812ffeb8e6fd1ddf65b33a512
HiroVodka <hiro.vodka06@gmail.com>
2021-11-16 22:50:39 +0900

    schema修正

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,22 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_111_141_127) do
-  create_table 'daily_reports', id: :bigint, unsigned: true,
-                                options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci', comment: '日報', force: :cascade do |t|
-    t.string 'title', null: false
-    t.text 'body', null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.datetime 'created_at', precision: 6, null: false
+ActiveRecord::Schema.define(version: 0) do
+
+  create_table "daily_reports", id: :bigint, unsigned: true, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", comment: "日報", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", precision: 6, null: false
   end
 
-  create_table 'users', options: 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci',
-                        force: :cascade do |t|
-    t.string 'email', null: false
-    t.string 'name', null: false
-    t.string 'password_digest', null: false
-    t.string 'auth_token', charset: 'utf8', collation: 'utf8_bin'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "name", null: false
+    t.string "password_digest", null: false
+    t.string "auth_token", collation: "utf8_bin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
