f1818a8f5d4222c0e02a134726dee11f8b3373aa
HiroVodka <hiro.vodka06@gmail.com>
2021-11-21 20:08:01 +0900

    seed用意

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+include FactoryBot::Syntax::Methods
+
+ApplicationRecord.transaction do
+  pp "---データ 作成---"
+  user = create(:user)
+  daily_report = create(:daily_report, user: user)
+  comment = create(:comment, user: user, daily_report: daily_report)
+  pp "---完了---"
+end
